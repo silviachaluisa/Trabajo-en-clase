3aa21720f8a6553543dad2197d06465a9573eb9e
Alejo-P <marcelo.pinzon@epn.edu.ec>
2023-11-21 09:24:17 -0500

    "vvalio"

--- main.java
+++ main.java
@@ -1,11 +1,10 @@
+import java.util.Scanner;
 //Comentario de prueba
 public class main {
     public static void main(String[] args)
     {
         Autos autos1=new Autos("Chevrolet");
         System.out.println("Modelo del carro "+autos1.getModelo());
-        System.out.println("Hola");
-        System.out.println("Silvia");
-        System.out.println("Anderson");
+        
     }
 }
